bc87f72d1f6e11c72528189632ab81a10b5c4dfb
TJ <tahmeenaowais@yahoo.com>
2020-10-20 23:41:07 -0400

    set routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,11 @@ var db = require("../models");
 
 // Route for getting some data about our user to be used client side
 module.exports= function(app){
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "../public/exercise.html"));
+  });
+  
 app.get("/all", function (req, res) {
  db. Workout.find({})
     .then(data => {
@@ -14,6 +19,45 @@ app.get("/all", function (req, res) {
     });
   
 });
+app.get("/api/workouts", function(req,res){
+db.Workout.find({}).then(data => {
+  res.json(data);
+})
+.catch(err => {
+  res.json(err);
+});
+});
+
+
+app.post("/api/workouts",(req, res) => {
+    db.Workout.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $set: {
+          type: req.body.type,
+          name: req.body.name,
+          modified: Date.now()
+        }
+      },
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  });
+  
+  // db.Workout.find({}).then(data => {
+  //   method: "POST";
+  //     headers: { "Content-Type"; "application/json" };
+  //     body: JSON.stringify(data)
+  //   });
+
+ 
 }
 // app.put("/api/workout_modify", function (req, res) {
 
