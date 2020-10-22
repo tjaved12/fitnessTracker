e524229586e0cfa7570f668e5ec66bdc9e834bbd
TJ <tahmeenaowais@yahoo.com>
2020-10-21 23:02:00 -0400

    fixed server.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -69,16 +69,7 @@ app.put("/api/workouts/:id",(req, res) => {
   });
 
    
-  // var condition = "id : " + req.params.id;
-  // console.log("condition", condition)
-  // db.Workout.update(condition, function(result) {
-  //   if (result.changedRows == 0) {
-  //     // If no rows were changed, then the ID must not exist, so 404
-  //     return res.status(404).end();
-  //   } else {
-  //     res.status(200).end();
-  //   }
-  // });
+ 
 });
  
   
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/Workout", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
