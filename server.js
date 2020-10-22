f72475be870d4ba5d89b1a7a3a52e9789bef80eb
TJ <tahmeenaowais@yahoo.com>
2020-10-21 22:09:30 -0400

    removed file

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
-const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
