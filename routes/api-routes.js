2db4128607473184c8b59af588879513e1571cd9
TJ <tahmeenaowais@yahoo.com>
2020-10-21 20:06:42 -0400

    working app

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
 const path = require("path");
+const { Workout } = require("../models");
 
 // Route for getting some data about our user to be used client side
 module.exports= function(app){
@@ -41,19 +42,43 @@ app.get("/api/workouts/range", function(req,res){
   });
   });
   
+  app.post("/api/workouts", function(req,res){
+    db.Workout.create({}).then(data=>{
+      console.log("data", data)
+      res.json(data)
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  })
 
 app.put("/api/workouts/:id",(req, res) => {
-   
-  var condition = "id = " + req.params.id;
-  console.log("condition", condition)
-  db.Workout.update(condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+
+  Workout.findOneAndUpdate(
+    { _id: req.params.id }, 
+    { $push: { exercises: req.body  } },
+
+  )
+
+.then(data=>{
+    console.log("data", data)
+    res.json(data)
+  })
+  .catch(err => {
+    res.json(err);
   });
+
+   
+  // var condition = "id : " + req.params.id;
+  // console.log("condition", condition)
+  // db.Workout.update(condition, function(result) {
+  //   if (result.changedRows == 0) {
+  //     // If no rows were changed, then the ID must not exist, so 404
+  //     return res.status(404).end();
+  //   } else {
+  //     res.status(200).end();
+  //   }
+  // });
 });
  
   
