94f4bd029e43e9afe6d27c06e6776a7d3a78d531
TJ <tahmeenaowais@yahoo.com>
2020-10-21 20:26:41 -0400

    fixed

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
+
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
